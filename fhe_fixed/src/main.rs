73710dad7708dc6798f6e8b001ea0af2557cb7cf
Márton Tot Bagi <mtotbagi@gmail.com>
2025-05-18 15:07:11 +0200

    switched to example usage in fhe_fixed

--- fhe_fixed/src/main.rs
+++ fhe_fixed/src/main.rs
@@ -1,43 +1,39 @@
-use std::io;
-use std::time::Instant;
-
-extern crate fixed as fixed_crate;
-
-#[allow(unused_imports)]
-use fixed_crate::types::{U0F16, U8F8, U10F6, U11F5, U12F4, U16F0};
-#[allow(unused_imports)]
-use fixed_crate::{FixedU8, FixedU16, FixedU128};
-#[allow(unused_imports)]
-use tfhe::integer::IntegerCiphertext;
-use tfhe::shortint::ClassicPBSParameters;
-use typenum::U1;
-#[allow(unused_imports)]
-use typenum::{
-    B0, B1, Bit, Cmp, Diff, IsGreater, IsGreaterOrEqual, PowerOfTwo, Same, True, U0, U2, U3, U4,
-    U5, U6, U8, U10, U11, U15, U16, U32, U1000, UInt, Unsigned,
-};
-
-pub const PARAM: ClassicPBSParameters =
-    tfhe::shortint::parameters::PARAM_MESSAGE_2_CARRY_2_KS_PBS_TUNIFORM_2M128;
-pub type Cipher = tfhe::integer::ciphertext::BaseRadixCiphertext<tfhe::shortint::Ciphertext>;
-
-mod fhe_testing_macros;
-mod fixed;
-mod tests;
-use crate::fixed::*;
+use fixed::types::U4F12;
+use tfhe::{FixedClientKey, FixedServerKey};
+use tfhe::FheU4F12;
 
 fn main() {
-    type FracType = U1;
-    type ClearFixed = FixedU8<FracType>;
-    test_func_manual!(U8, FracType, ck, server_key,          // Type of the operation, and key names
-        {
-            a.smart_round(&server_key)
-            // let mut res = a.clone();
-            // server_key.key.smart_div_assign_parallelized(res.inner.bits_mut(), b.inner.bits_mut());
-            // res
-        },               // The operation to test
-        ClearFixed::from_num(clear_a).int_log2(),               // A ground truth to compare to, optional
-        | clear_a, a |                      // The clear and encrypted name(s) of relevant variables
-        // iters                                        // The name(s) of variables that are only used as clear
-    );
+    
+    // Generate the client key and the server key:
+    let ckey = FixedClientKey::new();
+    let skey = FixedServerKey::new(&ckey);
+    
+    let clear_one: U4F12 = U4F12::from_num(1);
+    let clear_five: U4F12 = U4F12::from_num(5);
+    
+    // Encrypt:
+    let mut one = FheU4F12::encrypt(clear_one, &ckey);
+    let mut five = FheU4F12::encrypt(clear_five, &ckey);
+    
+    // Calculate the golden ratio:
+    let mut sqrt_five = five.smart_sqrt(&skey);
+    let mut one_plus_sqrt_five = one.smart_add(&mut sqrt_five, &skey);
+    let mut golden_ratio = one_plus_sqrt_five
+        .smart_div(&mut FheU4F12::encrypt_trivial(2, &skey), &skey);
+
+    
+    // Decrypt:
+    let dec_golden_ratio_precise: U4F12 = golden_ratio.decrypt(&ckey);
+    println!("Golden ratio to twelve bits of precision: {:.4}", dec_golden_ratio_precise);
+
+    // We truncate to keep the 4 most significant fractional bits
+    let golden_ratio_trunc = golden_ratio.smart_trunc(4, &skey);
+    
+    // Decrypt:
+    let dec_golden_ratio_trunc: U4F12 = golden_ratio_trunc.decrypt(&ckey);
+    println!("Golden ratio to four bits of precision: {:.4}", dec_golden_ratio_trunc);
+
+    let golden_ratio_round = golden_ratio.smart_round(&skey);
+    let clear_two: U4F12 = golden_ratio_round.decrypt(&ckey);
+    println!("Golden ratio rounded is two: {}", clear_two);
 }
